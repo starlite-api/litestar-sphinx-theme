c5ce66aadc8f910c24f54bf0d172798c237a67eb
Jacob Coffee <jacob@z7x.org>
2023-11-11 19:03:08 -0600

    meta: use community link for discord

--- litestar_sphinx_theme/__init__.py
+++ litestar_sphinx_theme/__init__.py
@@ -33,7 +33,7 @@ def update_html_context(
     context["github_repo_link"] = f"https://github.com/litestar-org/{theme_options['github_repo_name']}"
     context["discord_link"] = theme_options.get(
         "discord_link",
-        "https://discord.gg/X3FJqy8d2j",
+        "https://discord.gg/litestar-919193495116337154",
     )
     context["twitter_link"] = theme_options.get(
         "twitter_link",
@@ -71,7 +71,7 @@ def update_global_config(app: Sphinx) -> None:
             },
             {
                 "name": "Discord",
-                "url": "https://discord.gg/X3FJqy8d2j",
+                "url": "https://discord.gg/litestar-919193495116337154",
                 "icon": "fa-brands fa-discord",
                 "type": "fontawesome",
             },
